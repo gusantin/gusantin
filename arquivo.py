65496217092ae5a47844dd4eb02dc9b029accb3e
gusantin <gustavo.santinn@hotmail.com>
2026-04-14 11:05:20 -0300

    aaa

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,8 @@
 #abrir um arquivo para escrita
 with open('arquivo.txt', 'w') as file:
     file.write("Olá Mundo")
+
+# abrir um arquivo para leitura e escrita
+with open('arquivo.txt' , 'r+') as file:
+    conteudo = file.read()
+    file.write('\nAdicionando mais conteúdo')
