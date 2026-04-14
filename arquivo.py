9e374e6a1c064ae9f49dc80ebb98d845aa92f8f1
gusantin <gustavo.santinn@hotmail.com>
2026-04-14 11:21:17 -0300

    SASASAsasasasa

--- arquivo.py
+++ arquivo.py
@@ -11,3 +11,11 @@ with open('arquivo.txt' , 'r+') as file:
 with open('arquivo.txt', 'a') as file:
     file.write('\nMais uma linha no final do arquivo')
 
+
+#abre o arquivo no modo de escrita ('w')
+with open('arquivo.txt', 'w') as file:
+    #escreve no arquivo
+    file.write('linha 1\n')
+    file.write('Linha 2\n')
+    file.write('Linha 3\n')
+
