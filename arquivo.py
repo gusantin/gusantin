358aec689c32c71f9c263d37152eedcd2ebf8c4e
gusantin <gustavo.santinn@hotmail.com>
2026-04-14 11:26:46 -0300

    testetete

--- arquivo.py
+++ arquivo.py
@@ -19,3 +19,15 @@ with open('arquivo.txt', 'w') as file:
     file.write('Linha 2\n')
     file.write('Linha 3\n')
 
+#abre o arquivo no modo de leitura ('r')
+with open('arquivo.txt', 'r') as file:
+    #lê a primeira linha do arquivo
+    linha1 = file.readline()
+    #lê a segunda linha do arquivo
+    linha2 = file.readline()
+    #le a terceira linha do arquivo
+    linha3 = file.readline()
+    print(linha1)
+    print(linha2)
+    print(linha3)
+    
